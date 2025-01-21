20bfb2fcfa492a7a30f07c5552ab83723634ba03
Devarsh <75148774+webdev03@users.noreply.github.com>
2025-01-21 16:18:25 +1300

    Add compression algorithm

--- build.ts
+++ build.ts
@@ -9,10 +9,45 @@ const originalHTML = await minify(readFileSync("index.html").toString(), {
   removeComments: true,
   collapseWhitespace: true,
 });
-console.log(originalHTML);
 
-// TODO: Make this have cool compression
-const properHTML = originalHTML;
+// Compression algorithm
+// Originally made by AI because I didn't understand ReadableStreams and Uint8Arrays,
+// but I did rewrite a lot of it to minify and fix it
+async function compress(str: string) {
+  const compressedData = btoa(
+    String.fromCharCode(...new Uint8Array(Bun.deflateSync(str))),
+  );
+  return await minify(
+    `<script>
+      let bs = atob('${compressedData}');
+      let q = new Uint8Array(bs.length);
+      for (let i = 0; i < bs.length; i++) q[i] = bs.charCodeAt(i);
+      (async () => {
+        document.write(
+          await (new Response(new ReadableStream({
+            start(c) {
+              c.enqueue(q);
+              c.close();
+            }
+          }).pipeThrough(new DecompressionStream('deflate-raw'))
+          ).text())
+        )
+      })();
+    </script>`,
+    {
+      minifyJS: true,
+      removeComments: true,
+      collapseWhitespace: true,
+    },
+  );
+}
+
+const compressedHTML = await compress(originalHTML);
+
+const properHTML =
+  compressedHTML.length < originalHTML.length ? compressedHTML : originalHTML;
+
+console.log(properHTML);
 
 toFile("qr.png", `data:text/html,${properHTML}`, {
   type: "png",
